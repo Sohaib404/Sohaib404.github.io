22c2dac2aee0765f12d27253ed099a842d38d8b0
Sohaib Khadri <55634383+Sohaib404@users.noreply.github.com>
2021-01-15 20:37:54 -0700

    v2

--- README.md
+++
@@ -1,2 +0,0 @@
-# Sohaib404.github.io
-My website portfolio aswell as my first endeavor into web design.
--- main.js
+++ main.js
@@ -7,6 +7,19 @@ var btn = document.getElementById("about");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+let rocket = document.getElementById("rocket");
+document.addEventListener('mousemove', function(e) {
+      let rocketRects = rocket.getBoundingClientRect();
+      let rocketX = rocketRects.left + rocketRects.width / 2;
+      let rocketY = rocketRects.top + rocketRects.height / 2;
+      let x = e.pageX;
+      let y = e.pageY;
+      rocket.style.left = x + 'px';
+      rocket.style.top = y + 'px';
+      rocket.style.transform = "rotate(" + Math.atan2(y - rocketY, x - rocketX) + "rad)";
+})
+
+
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
@@ -23,4 +36,3 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
-
